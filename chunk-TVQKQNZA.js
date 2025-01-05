69c85790d8b253b02228cf44c5bd54b390baca9a
Maari1997 <mmarimuthu1997@gmail.com>
2025-01-05 13:17:29 +0530

    Auto-generated commit

---
+++ chunk-TVQKQNZA.js
@@ -0,0 +1,1 @@
+import{$a as o,_ as t,ab as i,hb as s,kb as a}from"./chunk-OU5XFULY.js";var p=class e{static \u0275fac=function(n){return new(n||e)};static \u0275cmp=t({type:e,selectors:[["app-sign-in"]],standalone:!0,features:[a],decls:2,vars:0,template:function(n,r){n&1&&(o(0,"p"),s(1,"sign-in works!"),i())}})};export{p as SignInComponent};
